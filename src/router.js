fe53e4a6e1ba2f3ae91a39e5ce107183759ad0e8
LordZerav <amrumuzan092@gmail.com>
2026-06-22 01:47:25 +0700

    Configure GitHub Pages deployment via Actions

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import CustomerHome from './views/CustomerHome.vue';
 import AdminLogin from './views/AdminLogin.vue';
 import AdminDashboard from './views/AdminDashboard.vue';
@@ -25,7 +25,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
